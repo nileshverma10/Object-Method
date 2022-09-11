853a4a91c8bfe3c7d362a4a2f86f6b10b7cc84e3
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2022-09-11 18:56:17 +0530

    Add files via upload

---
+++ mapNumber.js
@@ -0,0 +1,11 @@
+function mapChar(N) {
+    // Write code here
+    
+    let char ="abcdefghijklmnopqrstuvwxyz";
+        for(j=0; j<N; j++){
+           
+         console.log(char[j]+"-"+(j+1)); 
+    }
+
+}
+ 
